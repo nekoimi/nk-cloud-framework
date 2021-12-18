9965ff10ca2e2eee5cc7294389b3d1e896921b64
nekoimi <nekoimime@gmail.com>
2021-12-18 23:07:43 +0800

    update

---
+++ nk-framework/nk-mybatis/src/main/java/com/nekoimi/nk/framework/mybatis/handler/FixedDateTimeFieldMetaObjectHandler.java
@@ -0,0 +1,28 @@
+package com.nekoimi.nk.framework.mybatis.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * nekoimi  2021/7/2 下午3:40
+ */
+@Slf4j
+@Component
+public class FixedDateTimeFieldMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.debug("insert fill ...");
+        this.strictInsertFill(metaObject, "createdAt", LocalDateTime::now, LocalDateTime.class);
+        this.strictInsertFill(metaObject, "updatedAt", LocalDateTime::now, LocalDateTime.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.debug("update fill ...");
+        this.strictUpdateFill(metaObject, "updatedAt", LocalDateTime::now, LocalDateTime.class);
+    }
+}
