95b8a0f50e8c4254eca5033fb9fa6d209e958c17
nekoimi <nekoimime@gmail.com>
2021-12-19 17:46:25 +0800

    update

---
+++ nk-auth/src/main/java/com/nekoimi/nk/auth/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.nekoimi.nk.auth.user.service.impl;
+
+import com.nekoimi.nk.framework.mybatis.service.impl.ReactiveCrudServiceImpl;
+import com.nekoimi.nk.auth.user.entity.User;
+import com.nekoimi.nk.auth.user.mapper.UserMapper;
+import com.nekoimi.nk.auth.user.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * User Service
+ *
+ * nekoimi  2021-12-19
+ *
+ * This code is generated using nk-devtools.
+ */
+@Service
+public class UserServiceImpl extends ReactiveCrudServiceImpl<UserMapper, User> implements UserService {
+}
