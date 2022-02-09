80236f7f845e0adc62381264ac6fcd5c1ae8e3c6
nekoimi <nekoimime@gmail.com>
2022-02-09 11:48:31 +0800

    ext auto convert

--- nk-framework/nk-core/src/main/java/com/nekoimi/nk/framework/core/contract/AutoConvert.java
+++ nk-framework/nk-core/src/main/java/com/nekoimi/nk/framework/core/contract/AutoConvert.java
@@ -1,21 +1,41 @@
 package com.nekoimi.nk.framework.core.contract;
 
 import cn.hutool.core.convert.Convert;
+import cn.hutool.core.lang.TypeReference;
+
+import java.lang.reflect.Type;
 
 /**
  * nekoimi  2022/2/8 9:50
- *
+ * <p>
  * 自动类型转换接口
  */
 public interface AutoConvert {
 
     /**
-     * 默认类型转换实现
      * @param toType
      * @param <T>
      * @return
      */
     default <T> T convert(Class<T> toType) {
         return Convert.convert(toType, this);
+    }
+
+    /**
+     * @param reference
+     * @param <T>
+     * @return
+     */
+    default <T> T convert(TypeReference<T> reference) {
+        return Convert.convert(reference, this);
+    }
+
+    /**
+     * @param type
+     * @param <T>
+     * @return
+     */
+    default <T> T convert(Type type) {
+        return Convert.convert(type, this);
     }
 }
